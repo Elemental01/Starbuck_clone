3a4f4ddffd50eb53aca1177fc4bdc4d5297e0bd8
Elemental01 <raiharsh022@gmail.com>
2022-12-26 14:28:51 +0530

    Intital Commit

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import "./App.css";
+import Nav1 from "./Nav1";
+function App() {
+  return <Nav1></Nav1>;
+}
+
+export default App;
---
+++ src/Nav1.js
@@ -0,0 +1,13 @@
+function Nav1() {
+  return (
+    <div className="Nav1">
+      <nav>
+        <img src="./images/starbucks.png"></img>
+        <a href="./App.js"> Home</a>
+        <a href="./Order.js"> Order</a>
+        <a href="./Pay.js"> Pay</a>
+      </nav>
+    </div>
+  );
+}
+export default Nav1;
