12cb84272bccb08af6f620ff6b8e08eca2004882
Elemental01 <raiharsh022@gmail.com>
2022-12-27 19:06:05 +0530

    Adding Images and BarisRecomend.js, HndfCurt.js

---
+++ src/HndfCurt.js
@@ -0,0 +1,46 @@
+function HndfCurt() {
+  return (
+    <div>
+      <h2>Handcrafted Curations</h2>
+      <div className="hndf-Curt-Section">
+        <div className="hndf-item">
+          <span>
+            <img src="./images/Bestseller.jpg" alt="Bestseller"></img>
+          </span>
+          <p>Bestseller</p>
+        </div>
+        <div className="hndf-item">
+          <span>
+            <img src="./images/Drinks.jpg" alt="Bestseller"></img>
+          </span>
+          <p>Drinks</p>
+        </div>
+        <div className="hndf-item">
+          <span>
+            <img src="./images/Food.jpg" alt="Bestseller"></img>
+          </span>
+          <p>Food</p>
+        </div>
+        <div className="hndf-item">
+          <span>
+            <img src="./images/Merchandise.jpg" alt="Bestseller"></img>
+          </span>
+          <p>Merchandise</p>
+        </div>
+        <div className="hndf-item">
+          <span>
+            <img src="./images/CoffeeAtHome.jpg" alt="CoffeAtHome"></img>
+          </span>
+          <p>Coffe At Home</p>
+        </div>
+        <div className="hndf-item">
+          <span>
+            <img src="./images/ReadyToEat.jpg" alt="Eat"></img>
+          </span>
+          <p>Ready to Eat</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default HndfCurt;
